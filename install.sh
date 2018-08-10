6aab92f6632ea448101688b77508f31edb10c220
kb1000fx <724160351@qq.com>
2018-08-10 23:42:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+if ! command -v steamcmd > /dev/null 2>&1;then
+    echo "You have installed SteamCMD"
+
+else 
+    echo "You have not installed SteamCMD"
+    funInstallSteamCMD()
+fi  
+
 echo "Updating package list..."
 apt-get -y update
 apt-get -y upgrade
 
-echo "Installing required..."
-apt-get install lib32gcc1
-echo "Installing SteamCMD..."
-apt-get install steamcmd
-mkdir ~/Steam && cd ~/Steam
+function funInstallSteamCMD()
+{
+
+    echo "Installing required packages..."
+    apt-get install lib32gcc1
+    echo "Installing SteamCMD..."
+    apt-get install steamcmd
+    mkdir ~/Steam && cd ~/Steam
+    if [ "$?" -ne "0" ]; then
+        echo -e "\033[31m ERROR: Cannot install required packages! \033[0m" 
+        exit 1
+    fi
+    return 0
+}
+
+
+
