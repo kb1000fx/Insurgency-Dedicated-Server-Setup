d37bc6e3ac341cee227ec0465b1c0fdb66b28a3b
kb1000fx <724160351@qq.com>
2018-08-12 01:13:18 +0800

    completed

--- install_insurgency.sh
+++ install_insurgency.sh
@@ -1,22 +1,4 @@
-#!/bin/bash
-
-if [ `id -u` -eq 0 ];then
-    echo -e "\033[33m WARNING: Please don't run this script while operating as the root user for the security risk! \033[0m"
-    echo -e "\033[33m Press Ctrl+C to exit and change to a common user. Or press any key to continue.  \033[0m"
-    read
-fi
-
-echo "Updating package list..."
-apt-get -y update
-apt-get -y upgrade
-clear
-if [ ! command -v steamcmd > /dev/null 2>&1 ] && [ ! -f "steamcmd.sh" ];then
-    echo "You have not installed SteamCMD"
-    funInstallSteamCMD
-fi
-echo "Now you have installed SteamCMD, start to install Insurgency."
-mkdir ~/SteamServer && cd ~/SteamServer
-./steamcmd.sh +login anonymous +force_install_dir ./Insurgency2 +app_update 222880 +quit
+ #!/bin/bash
 
 function funInstallSteamCMD()
 {
@@ -33,6 +15,27 @@ function funInstallSteamCMD()
     clear
     return 0
 }
+
+if [ `id -u` -eq 0 ];then
+    echo -e "\033[33m WARNING: Please don't run this script while operating as the root user for the security risk! \033[0m"
+    echo -e "\033[33m Press Ctrl+C to exit and change to a common user. Or press any key to continue.  \033[0m"
+    read
+fi
+echo "Updating package list..."
+apt-get -y update
+apt-get -y upgrade
+clear
+if command -v steamcmd > /dev/null 2>&1;then
+	echo "Found steamcmd!" 
+elif [ -f "steamcmd.sh" ];then
+	echo "Found steamcmd.sh"
+else	
+	echo "You have not installed SteamCMD"
+	funInstallSteamCMD
+fi
+echo "Now you have installed SteamCMD, start to install Insurgency."
+./steamcmd.sh +login anonymous +force_install_dir ./Insurgency2 +app_update 222880 +quit
+
 
 
 
