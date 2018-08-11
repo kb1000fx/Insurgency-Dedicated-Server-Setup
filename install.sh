c3b014d09f7c15d3bd90ca6ac17a77629fd1ba2e
kb1000fx <724160351@qq.com>
2018-08-11 21:11:29 +0800

    update

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 if [ `id -u` -eq 0 ];then
     echo -e "\033[33m WARNING: Please don't run this script while operating as the root user for the security risk! \033[0m"
     echo -e "\033[33m Press Ctrl+C to exit and change to a common user. Or press any key to continue.  \033[0m"
+    read
 fi
+
 echo "Updating package list..."
 apt-get -y update
 apt-get -y upgrade
